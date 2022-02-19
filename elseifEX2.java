7d9dce3b9829950a7388ed425a66fae12465970e
jundohoon <100017431+jundohoon@users.noreply.github.com>
2022-02-19 17:13:16 +0900

    두 번쨰
    ㅇㅇㅇ

---
+++ elseifEX2.java
@@ -0,0 +1,13 @@
+package ex2;
+
+public class elseifEX2 {
+
+	public static void main(String[] args) {
+		int Score = 91;
+		if(Score > 90) {
+			char grade = 'A';
+			System.out.println("Score" + "이상은" + "grade" + "입니다" );
+		}
+	}
+
+}
